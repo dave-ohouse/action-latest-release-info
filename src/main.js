a51719cfcb24f2e065f5a15ea061953a83343472
dave-ohouse <ds.kim@bucketplace.net>
2022-03-15 17:50:18 +0900

    remove slack parts

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 const core = require("@actions/core");
 const {GitHub, context} = require("@actions/github");
-const axios = require('axios').default;
 
 async function run() {
     try {
         let githubToken = process.env.GITHUB_TOKEN;
         const github = new GitHub(githubToken);
-        const webhookUrl = process.env.SLACK_WEBHOOK_URL
 
         const {owner, repo} = context.repo;
         const releaseInfo = await github.repos.getLatestRelease({
@@ -14,27 +12,21 @@ async function run() {
             repo
         });
 
+        let releaseId = releaseInfo.data.id.toString();
         let htmlUrl = releaseInfo.data.html_url;
         let tagName = releaseInfo.data.tag_name;
         let name = releaseInfo.data.name;
         let body = releaseInfo.data.body;
 
-        console.log(`Got release info: '${name}', '${tagName}', '${htmlUrl}', ${body}`);
+        console.log(`Got release info: '${name}', '${tagName}', '${releaseId}', '${htmlUrl}', ${body}`);
 
-        axios.post(webhookUrl, {
-            "release.body": body,
-            "release.name": name,
-            "release.tag_name": tagName,
-            "html_url": htmlUrl
-        })
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-            core.setFailed(error.message);
-        });
-        
+        core.setOutput("id", releaseId);
+        core.setOutput("html_url", htmlUrl);
+        core.setOutput("tag_name", tagName);
+        core.setOutput("name", name);
+        core.setOutput("body", body)
+
+
     } catch (error) {
         console.log(error);
         core.setFailed(error.message);
