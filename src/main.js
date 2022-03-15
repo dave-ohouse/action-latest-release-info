72aa19a721fd63a5831ffd201653230270748a5d
dave-ohouse <ds.kim@bucketplace.net>
2022-03-15 17:04:20 +0900

    add slack web hook

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 const core = require("@actions/core");
 const {GitHub, context} = require("@actions/github");
+const axios = require('axios').default;
 
 async function run() {
     try {
         let githubToken = process.env.GITHUB_TOKEN;
         const github = new GitHub(githubToken);
+        const webhookUrl = process.env.SLACK_WEBHOOK_URL
 
         const {owner, repo} = context.repo;
         const releaseInfo = await github.repos.getLatestRelease({
@@ -12,23 +14,27 @@ async function run() {
             repo
         });
 
-        let releaseId = releaseInfo.data.id.toString();
         let htmlUrl = releaseInfo.data.html_url;
-        let uploadUrl = releaseInfo.data.upload_url;
         let tagName = releaseInfo.data.tag_name;
         let name = releaseInfo.data.name;
-        let createdAt = releaseInfo.data.created_at;
-        let draft = releaseInfo.data.draft
+        let body = releaseInfo.data.body;
 
-        console.log(`Got release info: '${name}', '${tagName}', '${releaseId}', '${htmlUrl}', '${uploadUrl}', '${createdAt}', 'draft:${draft}'`);
+        console.log(`Got release info: '${name}', '${tagName}', '${htmlUrl}', ${body}`);
 
-        core.setOutput("id", releaseId);
-        core.setOutput("html_url", htmlUrl);
-        core.setOutput("upload_url", uploadUrl);
-        core.setOutput("tag_name", tagName);
-        core.setOutput("created_at", createdAt);
-        core.setOutput("name", name);
-        core.setOutput("draft", draft)
+        axios.post(webhookUrl, {
+            "release.body": body,
+            "release.name": name,
+            "release.tag_name": tagName,
+            "html_url": htmlUrl
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+            core.setFailed(error.message);
+        });
+        
     } catch (error) {
         console.log(error);
         core.setFailed(error.message);
